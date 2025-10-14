119abe51373b4c9b82c1e98fca4404c068167435
yeeter-idk <115333503+yeeter-idk@users.noreply.github.com>
2025-10-14 18:31:33 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -6,14 +6,24 @@ let bCtx = buffer.getContext("2d");
 class letter {
   constructor(character, x, y) {
     this.character = character;
-    this.x = x;
-    this.y = y;
+    
+    let [left, top, right, bottom] = getExactRect(character);
+    
+    this.offsetX = left;
+    this.offsetY = top;
+    this.width = right - left;
+    this.height = bottom - top;
+    
+    this.x = x + this.offsetX;
+    this.y = y + this.offsetY;
     
     this.sx = 0;
     this.sy = 0;    
     
     this.startX = x;
     this.startY = y;
+    
+    this.mass = (this.width + this.height) / 2;
     
     this.freeze = true;
     
@@ -24,21 +34,41 @@ class letter {
     this.sy += 0.1;
     
     // centered
-    let cx = this.x + charW * 0.5; 
-    let cy = this.y + charH * 0.5;
+    let cx = this.x + this.width * 0.5; 
+    let cy = this.y + this.height * 0.5;
     
     let waterHeight = waterLevel * canvas.height;
     
-    let submerged = (this.y + charH * 1.25 - waterHeight) / charH;
+    let submerged = (this.y + this.height * 1.25 - waterHeight) / this.height;
     submerged = Math.max(0, Math.min(submerged, 1));
     
-    if((submerged - this.lastSub) * 2 > Math.random()){
+    if((submerged - this.lastSub) * 4 > Math.random()){
       bubbles.push(
         new bubble(
           cx, 
           waterHeight, 
           this.sx + (Math.random() - 0.5) * 6, 
           this.sy + (Math.random() - 0.5) * 6
+        )
+      );
+    }
+    
+    if(submerged != 0 && submerged != 1){
+      let speed = Math.hypot(this.sx, this.sy);
+      
+      if(speed < Math.random()) return;
+      
+      let force = Math.abs(submerged - this.lastSub) * speed;
+      
+      let lateral = this.sx + (Math.random() - 0.5) * force;
+      let vertical = -speed / 2;
+      
+      splashes.push(
+        new splash(
+          cx, 
+          waterHeight, 
+          lateral, 
+          vertical
         )
       );
     }
@@ -51,7 +81,8 @@ class letter {
     let dy = cy - mouse.y;
     let dist = Math.hypot(dx, dy);
     if(mouse.down && mouse.range >= dist){
-      let force = (mouse.range - dist) / mouse.range * 3;
+      let force = (mouse.range - dist) / mouse.range * 2;
+      //let force = (dist) / mouse.range * -2;
       
       this.sx += dx / dist * force;
       this.sy += dy / dist * force;
@@ -59,8 +90,8 @@ class letter {
       this.freeze = false;
     }
     
-    this.sx *= 1 - 0.1 * submerged;
-    this.sy *= 1 - 0.1 * submerged;
+    this.sx *= 1 - 0.15 * submerged;
+    this.sy *= 1 - 0.15 * submerged;
     
     if(this.freeze){
       this.sx = 0;
@@ -73,8 +104,8 @@ class letter {
       this.sx = Math.abs(this.sx) * boundBounce;  
       this.sy *= boundFriction;
     }
-    if(this.x + charW > canvas.width){
-      this.x = canvas.width - charW;
+    if(this.x + this.width > canvas.width){
+      this.x = canvas.width - this.width;
       this.sx = -Math.abs(this.sx) * boundBounce;  
       this.sy *= boundFriction;
     }
@@ -85,8 +116,8 @@ class letter {
       this.sx *= boundFriction;
       this.sy = Math.abs(this.sy) * boundBounce;  
     }
-    if(this.y + charH > canvas.height){
-      this.y = canvas.height - charH;
+    if(this.y + this.height > canvas.height){
+      this.y = canvas.height - this.height;
       this.sx *= boundFriction;
       this.sy = -Math.abs(this.sy) * boundBounce;
     }
@@ -99,36 +130,46 @@ class letter {
   }
   
   collide(other) {
-    if(!rectCol(this.x, this.y, charW, charH, other.x, other.y, charW, charH))
+    if(!rectCol(this.x, this.y, this.width, this.height, other.x, other.y, other.width, other.height))
       return;
       
     this.freeze = false;
     other.freeze = false;
       
-    let diffX = charW - Math.abs(this.x - other.x);
-    let diffY = charH - Math.abs(this.y - other.y);
+    let diffX = Math.min(
+      (this.x + this.width) - other.x,
+      (other.x + other.width) - this.x   
+    );
+    let diffY = Math.min(
+      (this.y + this.height) - other.y,
+      (other.y + other.height) - this.y     
+    );
+    
     let dirX = Math.sign(this.x - other.x);
     let dirY = Math.sign(this.y - other.y);
     
+    let a = other.mass / this.mass;
+    let b = this.mass / other.mass;
+    
     if(diffX < diffY){ // horiz
-      let displaced = diffX * dirX / 2;
-      this.x += displaced;
-      other.x -= displaced;   
+      let displaced = diffX * dirX * 0.8;
+      this.x += displaced * a;
+      other.x -= displaced * b;   
       
-      this.sx += displaced * 0.5;
-      other.sx -= displaced * 0.5; 
+      this.sx += displaced * 0.5 * a;
+      other.sx -= displaced * 0.5 * b; 
     }else{ // vert
-      let displaced = diffY * dirY / 2;
-      this.y += displaced;
-      other.y -= displaced;   
+      let displaced = diffY * dirY * 0.8;
+      this.y += displaced * a;
+      other.y -= displaced * b;   
       
-      this.sy += displaced * 0.5;
-      other.sy -= displaced * 0.5; 
+      this.sy += displaced * 0.5 * a;
+      other.sy -= displaced * 0.5 * b; 
     }
   }
   
   draw() {
-    ctx.fillText(this.character, this.x, this.y + charH);
+    ctx.fillText(this.character, this.x - this.offsetX, this.y + charH - this.offsetY);
   }
 }
 
@@ -139,11 +180,11 @@ class bubble {
     this.sx = sx;
     this.sy = sy;
     
-    this.radius = 1.5;
+    this.radius = 0.8;
   }
   
   update() {
-    this.sy -= 0.1;
+    this.sy -= 0.13;
     
     this.sx *= 0.9;
     this.sy *= 0.9;
@@ -164,6 +205,35 @@ class bubble {
   }
 }
 
+class splash {
+  constructor(x, y, sx, sy) {
+    this.x = x;
+    this.y = y;
+    this.sx = sx;
+    this.sy = sy;
+    
+    this.radius = 1.5;
+  }
+  
+  update() {
+    this.sy += 0.1;
+    
+    this.x += this.sx;
+    this.y += this.sy;
+    
+    if(this.y + this.radius > waterLevel * canvas.height)
+      return true;
+      
+    return false;
+  }
+  
+  draw() {
+    bCtx.beginPath();
+    bCtx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    bCtx.fill();
+  }
+}
+
 let fontSize = 24;
 
 let charW = 0;
@@ -172,7 +242,7 @@ let charH = 0;
 let boundBounce = 0.5;
 let boundFriction = 0.9;
 
-let waterLevel = 0.75;
+let waterLevel = 1;
 
 let mouse = {
   range: 80,
@@ -185,10 +255,11 @@ let frame = 1;
 
 let elements = [];
 let bubbles = [];
+let splashes = [];
 
-function startingPhrase(text) {
+function startingPhrase(text, yOffset) {
   let originX = canvas.width * 0.5 - (text.length * charW) * 0.5;
-  let originY = canvas.height * 0.5 - charH * 0.5;
+  let originY = canvas.height * 0.5 - charH * 0.5 + yOffset;
   
   for(let i = 0; i < text.length; i++){
     let character = text[i];
@@ -214,7 +285,10 @@ function setup() {
   charW = metrics.width;
   charH = metrics.fontBoundingBoxAscent + metrics.fontBoundingBoxDescent;
   
-  startingPhrase("Procrastination is a CRAZY drug.");
+  startingPhrase("Procrastination is a CRAZY drug.", 0);
+  
+  //startingPhrase("Procrastination is a CRAZY drug.", -charH * 0.6);
+  //startingPhrase("I don't want to do anything.", charH * 0.6);
     
   loop();
 }
@@ -227,6 +301,15 @@ function loop() {
     randElem.freeze = false;
   }
   
+  if(frame % 500 == 0){
+    startingPhrase("Laurence");
+  }
+  
+  let targetLevel = Math.min(frame / 50000 + 0.2, 1); 
+  
+  waterLevel = (1 - targetLevel) * 0.001 + waterLevel * 0.999;
+  
+  for(let p = 0; p < 1; p++)
   for(let i = 0; i < elements.length; i++)
     for(let j = i + 1; j < elements.length; j++)
       elements[i].collide(elements[j]);
@@ -247,30 +330,34 @@ function drawWater() {
   bCtx.clearRect(0, 0, canvas.width, canvas.height);
   bCtx.fillStyle = "#2babff";
   bCtx.strokeStyle = "#aadfff";
-  bCtx.lineWidth = 4;
   bCtx.globalAlpha = 0.3;
   
-  let waterHeight = waterLevel * canvas.height;
+  let waterHeight = Math.round(waterLevel * canvas.height);
   
   bCtx.fillRect(0, waterHeight, canvas.width, canvas.height);
-  
-  bCtx.beginPath();
-  bCtx.moveTo(0, waterHeight);
-  bCtx.lineTo(canvas.width, waterHeight);
-  bCtx.stroke();
   
   for(let i = 0; i < bubbles.length; i++) 
     if(bubbles[i].update()){
       bubbles.splice(i, 1);
       i--;
     }
+  
+  for(let i = 0; i < splashes.length; i++) 
+    if(splashes[i].update()){
+      splashes.splice(i, 1);
+      i--;
+    }
+  
+  for(let part of splashes) 
+    part.draw(); 
     
   bCtx.globalAlpha = 1;
   bCtx.globalCompositeOperation = "destination-out";
   for(let part of bubbles) 
     part.draw();
-  
   bCtx.globalCompositeOperation = "source-over";
+   
+   
   ctx.drawImage(buffer, 0, 0);
 }
 
---
+++ measuringText.js
@@ -0,0 +1,39 @@
+function getExactRect(character) {
+  let field = document.createElement("canvas");
+  let fCtx = field.getContext("2d");
+  
+  field.width = charW * 3;
+  field.height = charH * 3;
+  
+  fCtx.font = fontSize + "px 'Courier New'";
+  fCtx.fillText(character, charW, charH * 2);
+  
+  let top = 99999;
+  let bottom = 0;
+  let left = 99999;
+  let right = 0;
+  
+  let data = fCtx.getImageData(0, 0, field.width, field.height).data;
+  
+  for(let x = 0; x < field.width; x++){
+    for(let y = 0; y < field.height; y++){
+      if(getPixel(x, y) > 1){
+        top = Math.min(top, y);
+        bottom = Math.max(bottom, y);
+        left = Math.min(left, x);
+        right = Math.max(right, x);
+      }
+    }
+  }
+  
+  return [
+    left - charW, 
+    top - charH, 
+    right - charW + 1, 
+    bottom - charH + 1
+  ];
+  
+  function getPixel(x, y) {
+    return data[(x + y * field.width) * 4 + 3];
+  }
+}
